7b4d88efc3dc5e8083755cd020a8a1da0b50c973
Ron de Ridder <ron.deridder@wolfpack-dcs.com>
2023-01-17 17:08:36 +0100

    Added basic mechanism to get a token to request the process list.

--- PSManager/Components/ProcessListComponent.razor.cs
+++ PSManager/Components/ProcessListComponent.razor.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Identity.Web;
 using PSDTO.Process;
+using System.Net.Http.Headers;
 
 namespace PSManager.Components
 {
@@ -8,10 +10,14 @@ namespace PSManager.Components
         [Inject]
         private HttpClient HttpClient { get; set; }
 
+        [Inject]
+        private ITokenAcquisition TokenAcquisition { get; set; }
+
         private ProcessListDTO[]? processes;
 
         protected override async Task OnInitializedAsync()
         {
+            await PrepareAuthenticatedClient(HttpClient);
             processes = await HttpClient.GetFromJsonAsync<ProcessListDTO[]>("/PS/GetProcessList");
         }
 
@@ -20,5 +26,17 @@ namespace PSManager.Components
             var process = await HttpClient.GetFromJsonAsync<ProcessDTO>($"/PS/GetProcess?id={processId}");
             return process;
         }
+
+        private async Task PrepareAuthenticatedClient(HttpClient httpClient)
+        {
+            //You would specify the scopes (delegated permissions) here for which you desire an Access token of this API from Azure AD.
+            //Note that these scopes can be different from what you provided in startup.cs.
+            //The scopes provided here can be different or more from the ones provided in Startup.cs. Note that if they are different,
+            //then the user might be prompted to consent again.
+            var accessToken = await TokenAcquisition.GetAccessTokenForUserAsync(new List<string>());
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
     }
 }
